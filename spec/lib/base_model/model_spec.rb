0642d7c19d38a0020cbd2159e46c820daf8b2b8b
Jim Bailey <jbailey@ukcloud.com>
2019-11-26 14:10:10 +0000

    Add attribute tests

--- spec/lib/base_model/model_spec.rb
+++ spec/lib/base_model/model_spec.rb
@@ -27,10 +27,33 @@ RSpec.describe ActiveGraphQl::BaseModel::Model do
       end
     end
 
-    let(:model_instance) { model_klass.new }
+    let(:model_instance) { model_klass.new(initial_attributes) }
+    let(:initial_attributes) { {} }
 
     it 'returns the models attributes' do
       expect(model_instance.attributes).to eq(returned_attributes)
+    end
+
+    describe 'set an attribute' do
+      let(:name) { 'Name1' }
+
+      before { model_instance.name = name }
+
+      it 'sets the right value' do
+        expect(model_instance.name).to eq name
+      end
+
+      context 'with an initial attribute' do
+        let(:initial_attributes) do
+          { security_domain: security_domain }
+        end
+
+        let(:security_domain) { 'ASSURED' }
+
+        it 'sets the right value' do
+          expect(model_instance.security_domain).to eq security_domain
+        end
+      end
     end
   end
 end
